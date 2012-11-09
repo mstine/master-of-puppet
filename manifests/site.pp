c14d87197d371952c8944d7d2479f0a938d8a1fd
Matt Stine <matt.stine@gmail.com>
2012-11-09 05:43:41 +0000

    initial commit

---
+++ manifests/site.pp
@@ -0,0 +1,23 @@
+node 'agent' {
+
+notify { "Hello World!": }
+
+}
+
+node 'puppetdb' {
+
+   package { "puppetdb":
+      ensure => latest,
+   }
+
+   service { "puppetdb":
+      ensure   => running,
+      enable   => true,
+      require  => Package["puppetdb"],
+   }
+
+   service { "iptables":
+      ensure => stopped,
+   }
+
+}
