5093ab4ac351bbd30183ce2d3ee8704e2440a201
Matt Stine <matt.stine@gmail.com>
2012-11-09 10:51:18 -0600

    add initial tomcat module

---
+++ modules/tomcat/manifests/init.pp
@@ -0,0 +1,13 @@
+class { "tomcat":
+
+  package { "tomcat6":
+    ensure => latest,
+  }
+
+  service { "tomcat6":
+    ensure  => running,
+    enabled => true,
+    require => Package["tomcat6"],
+  }
+}
+
