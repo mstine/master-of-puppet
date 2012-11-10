bb5e7f3997fde12fc49d42f6d6159629c25a0127
Matt Stine <matt.stine@gmail.com>
2012-11-09 23:13:39 -0600

    fix typo

--- modules/mysql/manifests/db.pp
+++ modules/mysql/manifests/db.pp
@@ -5,7 +5,7 @@ define db($user, $password) {
   exec { "create-${name}-db":
     unless  => "/usr/bin/mysql -u${user} -p${password} ${name}",
     command => "/usr/bin/mysql -uroot -p${mysql_password} -e \"create database ${name}; grant all on ${name}.* to ${user}@'%' identified by '$password'; flush privileges;\"",
-    require => Service["mysql"],
+    require => Service["mysqld"],
   }
 
 }
