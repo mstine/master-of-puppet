60fbef5fa04f9af6976447a4c4a8d7ccf111caa1
Matt Stine <matt.stine@gmail.com>
2012-11-09 23:12:52 -0600

    fix typo

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -15,7 +15,7 @@ class mysql {
   exec { "set-mysql-password":
     unless  => "/usr/bin/mysqladmin -uroot -p${mysql_password} status",
     command => "/usr/bin/mysqladmin -uroot password ${mysql_password}",
-    require => Service["mysql"],
+    require => Service["mysqld"],
   }
 
   db { "todos":
